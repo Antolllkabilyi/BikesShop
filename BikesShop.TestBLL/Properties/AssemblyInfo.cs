5c2bf78537bd2406af7b686bde05cdd860ae3a86
Антон Белый <antonbilyiph@gmail.com>
2017-06-29 16:23:15 +0300

    add test project

---
+++ BikesShop.TestBLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BikesShop.TestBLL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BikesShop.TestBLL")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a06db4f-229a-4c3b-80c2-70b56c1c5562")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BikesShop.TestBLL/UnitTestColorService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BikesShop.BLL.DTO;
+using BikesShop.BLL.Interfaces;
+using BikesShop.BLL.Services;
+using BikesShop.DAL.Entities;
+using BikesShop.DAL.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ninject;
+
+namespace BikesShop.TestBLL
+{
+    [TestClass]
+    public class UnitTestColorService
+    {
+        private readonly Mock<IColorRepository> _colorRepositoryMock = new Mock<IColorRepository>();
+
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+        private ColorsService _service;
+
+        private IKernel _kernel;
+        [TestInitialize]
+        public void TestInit()
+        {
+            _kernel = new StandardKernel();
+            _kernel.Bind<IColorRepository>().ToConstant(_unitOfWorkMock.Object.BicycleColors);
+         
+        }
+
+        [TestMethod]
+        public void GetAll_Color()
+        {
+            BicycleColorDTO color = new BicycleColorDTO
+            {
+                Name = "Blue"
+            };
+
+            var mock = new Mock<IUnitOfWork>();
+           
+            _service = new ColorsService(mock.Object);
+            //_service.Create(color);
+          
+            _service.Create(color);
+            List<BicycleColorDTO> colors = _service.GetAll().ToList();
+            //Assert.IsTrue(isCreateCalled);
+            Assert.AreEqual(0,colors.Count);
+        }
+
+    }
+}
